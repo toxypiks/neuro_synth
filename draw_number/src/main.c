99740f3871ce2bb480f9b5f589f6228959c647e9
toxypiks <HannaHinkelbein89@gmx.de>
2026-03-12 13:09:30 +0100

    Rescale image when taking a screenshot

--- draw_number/src/main.c
+++ draw_number/src/main.c
@@ -9,13 +9,14 @@ int main()
     const int screenWidth = 800;
     const int screenHeight = 450;
 
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(screenWidth, screenHeight, "Draw a number");
 
     SetTargetFPS(60);
-    Vector2* points = NULL;
 
     Vector2** strokes = NULL;
     Vector2* current_stroke = NULL;
+    size_t screenshot_index = 0;
 
     while(!WindowShouldClose())
     {
@@ -35,7 +36,14 @@ int main()
 
         if(IsKeyPressed(KEY_S))
         {
-            TakeScreenshot("screenshot.png");
+            Image img = LoadImageFromScreen();
+            ImageResize(&img, 28, 28);
+            ImageColorGrayscale(&img);
+            char buffer[256];
+            sprintf(buffer, "screenshot_%d.png", screenshot_index);
+            ExportImage(img, buffer);
+            UnloadImage(img);
+            screenshot_index++;
         }
 
         if(IsKeyPressed(KEY_R))
@@ -54,14 +62,23 @@ int main()
         BeginDrawing();
         ClearBackground(BLACK);
 
+        float thickness = 20.0f;
+
         for (int i = 0; i < arrlen(strokes); i++)
         {
-            if (arrlen(strokes[i]) > 1)
-                DrawLineStrip(strokes[i], arrlen(strokes[i]), WHITE);
+            for (int j = 1; j < arrlen(strokes[i]); j++)
+            {
+                DrawLineEx(strokes[i][j-1], strokes[i][j], thickness, WHITE);
+            }
         }
 
-        if (current_stroke != NULL && arrlen(current_stroke) > 1)
-            DrawLineStrip(current_stroke, arrlen(current_stroke), WHITE);
+        if (current_stroke != NULL)
+        {
+            for (int j = 1; j < arrlen(current_stroke); j++)
+            {
+                DrawLineEx(current_stroke[j-1], current_stroke[j], thickness, WHITE);
+            }
+        }
 
         EndDrawing();
     }
