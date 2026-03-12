2a523bedb12c012266192f0e500d1e09eae7bf86
toxypiks <HannaHinkelbein89@gmx.de>
2026-03-12 14:01:03 +0100

    Use canvas to draw on and export

--- draw_number/src/main.c
+++ draw_number/src/main.c
@@ -9,22 +9,58 @@ int main()
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(screenWidth, screenHeight, "Draw a number");
-
     SetTargetFPS(60);
+
+    // drawing area
+    const int canvas_size = 280;
+    Rectangle canvas_rect = {260, 85, canvas_size, canvas_size};
+
+    RenderTexture2D canvas = LoadRenderTexture(canvas_size, canvas_size);
+
+    BeginTextureMode(canvas);
+    ClearBackground(BLACK);
+    EndTextureMode();
 
     Vector2** strokes = NULL;
     Vector2* current_stroke = NULL;
+
     size_t screenshot_index = 0;
 
-    while(!WindowShouldClose())
+    while (!WindowShouldClose())
     {
+        Vector2 mouse = GetMousePosition();
+
+        // drawing
         if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
         {
-            arrput(current_stroke, GetMousePosition());
-        }
+            if (CheckCollisionPointRec(mouse, canvas_rect))
+            {
+                Vector2 canvas_pos = {
+                    mouse.x - canvas_rect.x,
+                    mouse.y - canvas_rect.y
+                };
 
+                arrput(current_stroke, canvas_pos);
+
+                // draw on canvas
+                if (arrlen(current_stroke) > 1)
+                {
+                    BeginTextureMode(canvas);
+
+                    float thickness = 10.0f;
+
+                    DrawLineEx(
+                        current_stroke[arrlen(current_stroke)-2],
+                        current_stroke[arrlen(current_stroke)-1],
+                        thickness,
+                        WHITE
+                    );
+
+                    EndTextureMode();
+                }
+            }
+        }
         else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
         {
             if (current_stroke != NULL)
@@ -34,24 +70,34 @@ int main()
             }
         }
 
-        if(IsKeyPressed(KEY_S))
+        // screenshot (28x28 export)
+        if (IsKeyPressed(KEY_S))
         {
-            Image img = LoadImageFromScreen();
+            Image img = LoadImageFromTexture(canvas.texture);
+
+            ImageFlipVertical(&img);
             ImageResize(&img, 28, 28);
             ImageColorGrayscale(&img);
+
             char buffer[256];
-            sprintf(buffer, "screenshot_%d.png", screenshot_index);
+            sprintf(buffer, "digit_%zu.png", screenshot_index);
+
             ExportImage(img, buffer);
             UnloadImage(img);
+
             screenshot_index++;
         }
 
-        if(IsKeyPressed(KEY_R))
+        // reset
+        if (IsKeyPressed(KEY_R))
         {
+            BeginTextureMode(canvas);
+            ClearBackground(BLACK);
+            EndTextureMode();
+
             for (int i = 0; i < arrlen(strokes); i++)
-            {
                 arrfree(strokes[i]);
-            }
+
             arrfree(strokes);
             strokes = NULL;
 
@@ -59,36 +105,35 @@ int main()
             current_stroke = NULL;
         }
 
+        // rendering
         BeginDrawing();
-        ClearBackground(BLACK);
+        ClearBackground(DARKGRAY);
 
-        float thickness = 20.0f;
+        DrawText("Draw a digit", 20, 20, 20, WHITE);
+        DrawText("S = Save 28x28", 20, 50, 20, WHITE);
+        DrawText("R = Reset", 20, 80, 20, WHITE);
 
-        for (int i = 0; i < arrlen(strokes); i++)
-        {
-            for (int j = 1; j < arrlen(strokes[i]); j++)
-            {
-                DrawLineEx(strokes[i][j-1], strokes[i][j], thickness, WHITE);
-            }
-        }
+        DrawRectangleLines(canvas_rect.x-1, canvas_rect.y-1, canvas_rect.width+2, canvas_rect.height+2, WHITE);
 
-        if (current_stroke != NULL)
-        {
-            for (int j = 1; j < arrlen(current_stroke); j++)
-            {
-                DrawLineEx(current_stroke[j-1], current_stroke[j], thickness, WHITE);
-            }
-        }
+        DrawTexturePro(
+            canvas.texture,
+            (Rectangle){0, 0, canvas_size, -canvas_size},
+            canvas_rect,
+            (Vector2){0,0},
+            0,
+            WHITE
+        );
 
         EndDrawing();
     }
 
-    for (int i = 0; i < arrlen(strokes); i++) {
+    for (int i = 0; i < arrlen(strokes); i++)
         arrfree(strokes[i]);
-    }
+
     arrfree(strokes);
     arrfree(current_stroke);
 
+    UnloadRenderTexture(canvas);
     CloseWindow();
 
     return 0;
